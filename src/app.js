d26cd959a473ddb2924dcb00c876789056ea54a6
Malik DeJean <mdejean@MacBook-Pro.local>
2020-09-17 09:10:55 -0700

    building template

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
+const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
@@ -14,7 +15,18 @@ app.use(morgan(morganOption))
 app.use(helmet())
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
 app.use(cors())
 
