65cdf23282e4754e109d1c81f622fae6d3452594
Malik DeJean <mdejean@MacBook-Pro.local>
2020-10-15 10:20:23 -0700

    changes seeding and migration

--- migrations/002.do.create_fighters.sql
+++ migrations/002.do.create_fighters.sql
@@ -7,9 +7,6 @@ CREATE TABLE fighters (
     wins INT DEFAULT 0 NOT NULL,
     losses INT DEFAULT 0 NOT NULL,
     styleId INT REFERENCES fighting_styles(id) ON DELETE CASCADE NOT NULL,
-    attack INT NOT NULL,
-    defense INT NOT NULL,
-    speed INT NOT NULL,
     health INT NOT NULL,
     stamina INT NOT NULL
 );
--- seeds/seed.techFighter_tables.sql
+++ seeds/seed.techFighter_tables.sql
@@ -15,18 +15,14 @@ VALUES
     ('Wrestling'),
     ('Brazilian Jiu-Jitsu');
 
-INSERT INTO fighters(fighter_name, hometown, height, fighter_weight, wins, losses, styleId,  attack, defense, speed , health, stamina)
+INSERT INTO fighters(fighter_name, hometown, height, fighter_weight, wins, losses, styleId, health, stamina)
 VALUES
-    ('Fighter 1', 'Los Angeles, CA', 182, 185, 22, 0, 1, 20, 50, 50, 100, 100),
-    ('Fighter 2', 'New York, NY', 163, 145, 22, 3, 2, 20, 50, 50, 100, 125),
-    ('Fighter 3', 'London, England', 175, 200, 30, 1, 3, 20, 50, 50, 100, 90),
-    ('Fighter 4', 'Dallax, TX', 190, 225, 10, 0, 3, 20, 50, 50, 100, 110),
-    ('Fighter 5', 'Miami, FL', 170, 185, 15, 12, 4, 20, 50, 50, 100, 100),
-    ('Fighter 6', 'Mexico City, Mexico', 168, 135, 43, 0, 1, 20, 50, 50, 100, 125),
-    ('Fighter 7', 'Rio De Janiero, Brazil', 177, 165, 5, 1, 6, 20, 50, 50, 100, 100),
-    ('Fighter 8', 'Kingston, Jamaica', 170, 175, 12, 5, 2, 20, 50, 50, 100, 110),
-    ('Fighter 9', 'Hong Kong, China', 165, 155, 22, 0, 3, 20, 50, 50, 100, 90),
-    ('Fighter 10', 'Bangkok, Thailand', 169, 145, 97, 19, 2, 20, 50, 50, 100, 100);
+    ('Fighter 1', 'Los Angeles, CA', 182, 185, 22, 0, 1, 100, 100),
+    ('Fighter 2', 'New York, NY', 163, 145, 22, 3, 2, 100, 125),
+    ('Fighter 3', 'London, England', 175, 200, 30, 1, 3, 100, 90),
+    ('Fighter 4', 'Dallax, TX', 190, 225, 10, 0, 4, 100, 110),
+    ('Fighter 5', 'Miami, FL', 170, 185, 15, 12, 5, 100, 100),
+    ('Fighter 6', 'Mexico City, Mexico', 168, 135, 43, 0, 6, 100, 125);
 
 INSERT INTO attacks(attack_name, damage, energy_cost, styleId)
 VALUES
